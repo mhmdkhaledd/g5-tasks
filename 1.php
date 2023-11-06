c002318328b81b1877e78020a47ec659430e743a
khellooo <khellooo811@gmail.com>
2023-11-06 23:56:29 +0200

    first commit

---
+++ 1.php
@@ -0,0 +1,12 @@
+<?php
+$num = 5;
+ function checkEvenOrOdd($num){
+if ($num % 2 == 0) {
+    echo "The number is even.";
+} else {
+    echo "The number is odd.";
+}
+ }
+ checkEvenOrOdd($num);
+
+?>
